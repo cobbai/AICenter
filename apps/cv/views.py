5935bc85e31466a34bb2aaa457bac609d2b2dd64
baibin <xjhmbb@126.com>
2023-03-06 18:53:15 +0800

    version1

--- apps/cv/views.py
+++ apps/cv/views.py
@@ -9,3 +9,13 @@ cv_bp = Blueprint("cv", __name__, url_prefix='/cv')
 @cv_bp.route('/OCR/', methods=['GET', 'POST'])
 def OCR():
     return "ok"
+
+
+@cv_bp.route('/ImageClassification/', methods=['GET', 'POST'])
+def ImageClassification():
+    return "ok"
+
+
+@cv_bp.route('/ObjectDetection/', methods=['GET', 'POST'])
+def ObjectDetection():
+    return "ok"
